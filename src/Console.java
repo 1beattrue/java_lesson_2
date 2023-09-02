82e0b9f2feb15abf4c7edf0ce7704709cb3b685a
Vitaly <onebeattrue@gmail.com>
2023-09-02 04:10:47 +0300

    Задание 10

---
+++ src/Console.java
@@ -0,0 +1,6 @@
+public class Console {
+    // Задание 10
+    public static void main(String[] args) {
+        System.out.println(args.length);
+    }
+}
--- src/reverse/Tester.java
+++ src/reverse/Tester.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Tester {
     public static void main(String[] args) {
+        // Задание 8
         MyArray testArray = new MyArray(new String[]{"a", "b", "c", "d", "e"});
         System.out.println(Arrays.toString(testArray.reverse()));
     }
